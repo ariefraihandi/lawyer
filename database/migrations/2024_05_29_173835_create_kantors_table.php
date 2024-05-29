f169e1d0cddc0c93560dbbcacd6af4c2e5b7d060
ariefraihandi <raihandi93@gmail.com>
2024-05-30 00:41:19 +0700

    db kantor

---
+++ database/migrations/2024_05_29_173835_create_kantors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKantorsTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('kantors', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_kantor');
+            $table->string('email_kantor')->unique();
+            $table->string('hp_whatsapp');
+            $table->date('tanggal_pendirian');
+            $table->string('alamat');
+            $table->string('kode_pos');
+            $table->string('provinsi');
+            $table->string('kabupaten_kota');
+            $table->string('kecamatan');
+            $table->string('desa');
+            $table->string('website')->nullable();
+            $table->text('slogan')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('dokumen_pendirian')->nullable();
+            $table->boolean('agreement')->default(false);
+            $table->integer('paket')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('kantors');
+    }
+}
